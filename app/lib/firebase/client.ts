4c254add07b0c77abef6d3523f6c0fe5837b4470
Yuri Henrique <yurihenrique98@outlook.com>
2026-02-26 20:00:16 +0000

    docs: finalized setup instructions for new users

--- app/lib/firebase/client.ts
+++ app/lib/firebase/client.ts
@@ -2,6 +2,8 @@ import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+console.log("DEBUG - API KEY:", process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
